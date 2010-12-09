39588668e126d82a5ae80ca5e6e3fdfc6bce1a7f
Randy Stauner <randy@magnificent-tears.com>
2010-12-09 15:52:50 -0700

    Also skip the No404s test if AUTOMATED_TESTING is set
    use 'dzil smoke' instead of 'dzil test'

--- lib/Dist/Zilla/Plugin/PodLinkTests.pm
+++ lib/Dist/Zilla/Plugin/PodLinkTests.pm
@@ -65,6 +65,19 @@ The tests check for the following C<%ENV> variables:
 * C<$ENV{SKIP_POD_LINK_TESTS}> - skip both
 * C<$ENV{SKIP_POD_LINKCHECK}>  - skip L<Test::Pod::LinkCheck>
 * C<$ENV{SKIP_POD_NO404S}>     - skip L<Test::Pod::No404s>
+* C<$ENV{AUTOMATED_TESTING}>   - skip L<Test::Pod::No404s>
+
+I elected to skip the 404 test with C<AUTOMATED_TESTING>
+because I don't want to run that test (and bother the network) often,
+but I do like to run my author and release tests
+before actually attempting C<dzil release>.
+
+So using C<dzil smoke> instead of C<dzil test>
+will skip the 404 network tests.
+
+B<NOTE> You may need to update your CPANPLUS index
+before Test::Pod::LinkCheck will work (or in my case even install).
+Using the C<x> command at the C<cpanp> prompt did the trick for me.
 
 =head1 SEE ALSO
 
@@ -104,6 +117,7 @@ use Test::More;
 foreach my $env_skip ( qw(
 	SKIP_POD_LINK_TESTS
 	SKIP_POD_NO404S
+	AUTOMATED_TESTING
 ) ){
 	plan skip_all => "\$ENV{$env_skip} is set, skipping"
 		if $ENV{$env_skip};
