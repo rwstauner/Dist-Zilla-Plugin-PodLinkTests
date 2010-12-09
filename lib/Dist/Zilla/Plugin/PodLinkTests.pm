d9d46736d055b45c75d58a95b1d16dd377a480c2
Randy Stauner <randy@magnificent-tears.com>
2010-12-09 15:39:39 -0700

    Add a release test for Test::Pod::No404s
    Which tests to run is configurable; the default it 'both'.

--- lib/Dist/Zilla/Plugin/PodLinkTests.pm
+++ lib/Dist/Zilla/Plugin/PodLinkTests.pm
@@ -5,17 +5,50 @@ package Dist::Zilla::Plugin::PodLinkTests;
 
 	# dist.ini
 	[PodLinkTests]
+	; test = both   ; options: qw(both none linkcheck no404s)
 
 =cut
 
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
 
+my @tests = qw(linkcheck no404s);
+{
+	use Moose::Util::TypeConstraints 1.01;
+
+	has test => (
+		is      => 'ro',
+		isa     => enum( [ qw(both none), @tests ]),
+		default => 'both',
+	);
+
+	no Moose::Util::TypeConstraints;
+}
+
+# overwrite this sub imported from Data::Section
+# to only return desired sections.
+sub merged_section_data {
+	my ($self) = @_;
+
+	return {} if $self->test eq 'none';
+
+	my $data = $self->SUPER::merged_section_data;
+	return $data if $self->test eq 'both';
+
+	foreach my $test ( @tests ){
+		$self->test eq $test
+			or delete $data->{"xt/release/pod-$test.t"};
+	}
+
+	return $data;
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+=for Pod::Coverage merged_section_data
 
 =head1 DESCRIPTION
 
@@ -23,11 +56,20 @@ This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
 following files:
 
   xt/release/pod-linkcheck.t - a standard Test::Pod::LinkCheck test
+  xt/release/pod-no404s.t    - a standard Test::Pod::No404s test
+
+The tests check for the following C<%ENV> variables:
+
+=for :list
+* C<$ENV{SKIP_POD_LINK_TESTS}> - skip both
+* C<$ENV{SKIP_POD_LINKCHECK}>  - skip L<Test::Pod::LinkCheck>
+* C<$ENV{SKIP_POD_NO404S}>     - skip L<Test::Pod::No404s>
 
 =head1 SEE ALSO
 
 =for :list
 * L<Test::Pod::LinkCheck>
+* L<Test::Pod::No404s>
 
 =cut
 
@@ -38,8 +80,7 @@ use strict; use warnings;
 
 use Test::More;
 
-{
-	my $env_skip = 'SKIP_POD_LINK_TESTS';
+foreach my $env_skip ( qw(SKIP_POD_LINK_TESTS SKIP_POD_LINKCHECK) ){
 	plan skip_all => "\$ENV{$env_skip} is set, skipping"
 		if $ENV{$env_skip};
 }
@@ -49,4 +90,21 @@ if ( $@ ) {
 	plan skip_all => 'Test::Pod::LinkCheck required for testing POD';
 } else {
 	Test::Pod::LinkCheck->new->all_pod_ok;
+}
+___[ xt/release/pod-no404s.t ]___
+#!/usr/bin/perl
+use strict; use warnings;
+
+use Test::More;
+
+foreach my $env_skip ( qw(SKIP_POD_LINK_TESTS SKIP_POD_NO404S) ){
+	plan skip_all => "\$ENV{$env_skip} is set, skipping"
+		if $ENV{$env_skip};
+}
+
+eval "use Test::Pod::No404s";
+if ( $@ ) {
+	plan skip_all => 'Test::Pod::No404s required for testing POD';
+} else {
+	all_pod_files_ok();
 }
