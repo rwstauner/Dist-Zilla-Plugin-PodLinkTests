05de90f9f7e3bdc75077a6d154f80c45f7aab13f
Randy Stauner <randy@magnificent-tears.com>
2011-04-14 22:20:52 -0700

    Add vim modeline and reformat to match coding style

--- lib/Dist/Zilla/Plugin/PodLinkTests.pm
+++ lib/Dist/Zilla/Plugin/PodLinkTests.pm
@@ -1,3 +1,4 @@
+# vim: set ts=2 sts=2 sw=2 expandtab smarttab:
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::PodLinkTests;
@@ -25,42 +26,42 @@ extends 'Dist::Zilla::Plugin::InlineFiles';
 
 my @tests = qw(linkcheck no404s);
 {
-	use Moose::Util::TypeConstraints 1.01;
+  use Moose::Util::TypeConstraints 1.01;
 
-	has test => (
-		is      => 'ro',
-		isa     => enum( [ qw(both none), @tests ]),
-		default => 'both',
-	);
+  has test => (
+    is      => 'ro',
+    isa     => enum( [ qw(both none), @tests ]),
+    default => 'both',
+  );
 
-	no Moose::Util::TypeConstraints;
+  no Moose::Util::TypeConstraints;
 }
 
 # overwrite this sub imported from Data::Section
 # to only return desired sections.
 sub merged_section_data {
-	my ($self) = @_;
+  my ($self) = @_;
 
-	my $pre = 'Dist::Zilla::Plugin::Test::Pod::';
-	$self->log($_) for (
-		'!',
-		__PACKAGE__ . " is deprecated.",
-		"Use ${pre}LinkCheck and ${pre}No404s instead.",
-		'!',
-	);
+  my $pre = 'Dist::Zilla::Plugin::Test::Pod::';
+  $self->log($_) for (
+    '!',
+    __PACKAGE__ . " is deprecated.",
+    "Use ${pre}LinkCheck and ${pre}No404s instead.",
+    '!',
+  );
 
-	my $selftest = $self->test;
-	return {} if $selftest eq 'none';
+  my $selftest = $self->test;
+  return {} if $selftest eq 'none';
 
-	my $data = $self->SUPER::merged_section_data();
-	return $data if $selftest eq 'both';
+  my $data = $self->SUPER::merged_section_data();
+  return $data if $selftest eq 'both';
 
-	foreach my $test ( @tests ){
-		$selftest eq $test
-			or delete $data->{"xt/release/pod-$test.t"};
-	}
+  foreach my $test ( @tests ){
+    $selftest eq $test
+      or delete $data->{"xt/release/pod-$test.t"};
+  }
 
-	return $data;
+  return $data;
 }
 
 __PACKAGE__->meta->make_immutable;
